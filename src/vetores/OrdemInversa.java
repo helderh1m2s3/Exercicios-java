908800331b3b85e898db861f54f98d06221e3659
Helder Martins de Sousa <heldermartinsdesousa@gmail.com>
2023-02-24 16:39:17 -0300

    Adicionado OrdemInversa

--- src/vetores/OrdemInversa.java
+++ src/vetores/OrdemInversa.java
@@ -1,5 +1,20 @@
 package vetores;
 
 public class OrdemInversa {
-    
+    public static void main(String[] args) {
+        
+        int[] vetor = { 5, 3, 8, 9, 1, 12 };
+
+        System.out.print("Vetor Normal: ");
+        int count = 0;
+        while (count < (vetor.length)) {
+            System.out.print(vetor[count] + ", ");
+            count++;
+        }
+        
+        System.out.print("\n" + "Vetor Invertido: ");
+        for(int i = (vetor.length - 1); i >= 0;i--){
+            System.out.print(vetor[i] + ", ");
+        }
+    }
 }
